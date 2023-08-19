e9c6d341a9bdda34dd115f9a888cc6e7e30e3114
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-08-19 17:00:50 +0200

    add toolbar debug

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('api/v1/posts/', include('posts.api.v1.urls')),
     path('api/v1/users/', include('users.urls')),
 
-    # path('__debug__/', include('debug_toolbar.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui',),
     path('swagger/api.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
