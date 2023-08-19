267a0eef491c5434cdf1e3b6816f43395d563b01
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-08-19 17:50:36 +0200

    conftest file

---
+++ blog/tests/conftest.py
@@ -0,0 +1,54 @@
+import os
+import shutil
+
+import pytest
+from django.conf import settings
+from pytest_factoryboy import register
+from rest_framework.test import APIClient
+
+from tests.factory import CategoryFactory, LikeFactory, PostFactory, UserFactory, CommentFactory
+
+register(UserFactory)
+register(CategoryFactory)
+register(PostFactory)
+register(LikeFactory)
+register(CommentFactory)
+
+
+@pytest.fixture
+def media_root():
+    path = os.path.join(settings.BASE_DIR, 'tests/media/')
+    os.makedirs(path, exist_ok=True)
+    settings.MEDIA_ROOT = path
+    yield
+    shutil.rmtree(path)
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture()
+def create_user(db, user_factory):
+    return user_factory.create()
+
+
+@pytest.fixture()
+def create_post(db, post_factory):
+    return post_factory.create()
+
+
+@pytest.fixture()
+def create_category(db, category_factory):
+    return category_factory.create()
+
+
+@pytest.fixture()
+def create_author(db, user_factory):
+    return user_factory.create()
+
+
+@pytest.fixture()
+def create_comment(db, comment_factory, create_post, create_user):
+    return comment_factory.create(comment_user=create_user, comment_post=create_post)
